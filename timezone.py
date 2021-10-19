61724940ae97aae9eaf273147e6abb0768a567da
Brandon Gant <bgant@protonmail.com>
2021-10-19 15:23:24 -0500

    rename from dst.py to timezone.py

--- timezone.py
+++ timezone.py
@@ -1,6 +1,6 @@
 # Brandon Gant
 # Created: 2021-10-13
-# Updated: 2021-10-15
+# Updated: 2021-10-19
 #
 # Sources:
 #   Peter Hinch: https://forum.micropython.org/viewtopic.php?t=3675#p28989
@@ -12,18 +12,17 @@
 #
 # Usage:
 #   import time
-#   from dst import dst
-#   time.localtime(dst())
+#   from timezone import tz
+#   time.localtime(tz())
 #
 
 UTC_Offset_ST  = -6  # CST
 UTC_Offset_DST = -5  # CDT
 
 import time
-def dst():
+def tz():
     t = time.time()
     year = time.localtime(t)[0]
     start = time.mktime((year, 3,(14-(int(5*year/4+1))%7),2,0,0,0,0))  # 2AM the Second Sunday in March
     end   = time.mktime((year,11,( 7-(int(5*year/4+1))%7),2,0,0,0,0))  # 2AM the  First Sunday in November
     return t + (3600 * UTC_Offset_ST) if t < start or t > end else t + (3600 * UTC_Offset_DST)
-
